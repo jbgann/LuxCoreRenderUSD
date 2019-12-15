911505e8a1c117b7337fc9ac08aef8c9b293fbd9
Kate Kligman <kate@katekligman.com>
2019-12-14 19:56:26 -0800

    Add boilerplate renderPass

--- pxr/imaging/plugin/hdLuxCore/renderPass.cpp
+++ pxr/imaging/plugin/hdLuxCore/renderPass.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2016 Pixar and John Gann
+// Copyright 2016 Pixar
 //
 // Licensed under the Apache License, Version 2.0 (the "Apache License")
 // with the following modification; you may not use this file except in
@@ -24,17 +24,22 @@
 #include "pxr/imaging/glf/glew.h"
 
 #include "pxr/imaging/hd/renderPassState.h"
-#include "pxr/imaging/HdLuxCore/renderDelegate.h"
-#include "pxr/imaging/HdLuxCore/renderPass.h"
+#include "pxr/imaging/hdLuxCore/renderDelegate.h"
+#include "pxr/imaging/hdLuxCore/renderPass.h"
+
+#include <iostream>
+using namespace std;
 
 PXR_NAMESPACE_OPEN_SCOPE
 
 HdLuxCoreRenderPass::HdLuxCoreRenderPass(HdRenderIndex *index,
                                        HdRprimCollection const &collection,
                                        HdRenderThread *renderThread,
+                                       HdLuxCoreRenderer *renderer,
                                        std::atomic<int> *sceneVersion)
     : HdRenderPass(index, collection)
     , _renderThread(renderThread)
+    , _renderer(renderer)
     , _sceneVersion(sceneVersion)
     , _lastSceneVersion(0)
     , _lastSettingsVersion(0)
@@ -47,10 +52,12 @@ HdLuxCoreRenderPass::HdLuxCoreRenderPass(HdRenderIndex *index,
     , _depthBuffer(SdfPath::EmptyPath())
     , _converged(false)
 {
+    cout << "HdLuxCoreRenderPass::HdLuxCoreRenderPass()\n";
 }
 
 HdLuxCoreRenderPass::~HdLuxCoreRenderPass()
 {
+    cout << "HdLuxCoreRenderPass::~HdLuxCoreRenderPass()\n";
     // Make sure the render thread's not running, in case it's writing
     // to _colorBuffer/_depthBuffer.
     _renderThread->StopRender();
@@ -59,7 +66,21 @@ HdLuxCoreRenderPass::~HdLuxCoreRenderPass()
 bool
 HdLuxCoreRenderPass::IsConverged() const
 {
-    //TODO:Define convergence function for luxcore
+    cout << "HdLuxCoreRenderPass::IsConverged()\n";
+    // If the aov binding array is empty, the render thread is rendering into
+    // _colorBuffer and _depthBuffer.  _converged is set to their convergence
+    // state just before blit, so use that as our answer.
+    if (_aovBindings.size() == 0) {
+        return _converged;
+    }
+
+    // Otherwise, check the convergence of all attachments.
+    for (size_t i = 0; i < _aovBindings.size(); ++i) {
+        if (_aovBindings[i].renderBuffer &&
+            !_aovBindings[i].renderBuffer->IsConverged()) {
+            return false;
+        }
+    }
     return true;
 }
 
@@ -67,7 +88,155 @@ void
 HdLuxCoreRenderPass::_Execute(HdRenderPassStateSharedPtr const& renderPassState,
                              TfTokenVector const &renderTags)
 {
-   //TODO: Start renderThread
+    cout << "HdLuxCoreRenderPass::_Execute()\n";
+    // XXX: Add collection and renderTags support.
+    // XXX: Add clip planes support.
+
+    // Determine whether the scene has changed since the last time we rendered.
+    bool needStartRender = false;
+    int currentSceneVersion = _sceneVersion->load();
+    if (_lastSceneVersion != currentSceneVersion) {
+        cout << "HdLuxCoreRenderPass::_Execute -> needStartRender = true";
+        needStartRender = true;
+        _lastSceneVersion = currentSceneVersion;
+    }
+
+    // Likewise the render settings.
+    HdRenderDelegate *renderDelegate = GetRenderIndex()->GetRenderDelegate();
+    int currentSettingsVersion = renderDelegate->GetRenderSettingsVersion();
+    if (_lastSettingsVersion != currentSettingsVersion) {
+        _renderThread->StopRender();
+        _lastSettingsVersion = currentSettingsVersion;
+
+        _renderer->SetSamplesToConvergence(
+            renderDelegate->GetRenderSetting<int>(
+                HdRenderSettingsTokens->convergedSamplesPerPixel, 1));
+
+        bool enableAmbientOcclusion =
+            renderDelegate->GetRenderSetting<bool>(
+                HdLuxCoreRenderSettingsTokens->enableAmbientOcclusion, false);
+        if (enableAmbientOcclusion) {
+            _renderer->SetAmbientOcclusionSamples(
+                renderDelegate->GetRenderSetting<int>(
+                    HdLuxCoreRenderSettingsTokens->ambientOcclusionSamples, 0));
+        } else {
+            _renderer->SetAmbientOcclusionSamples(0);
+        }
+
+        _renderer->SetEnableSceneColors(
+            renderDelegate->GetRenderSetting<bool>(
+                HdLuxCoreRenderSettingsTokens->enableSceneColors, true));
+
+        needStartRender = true;
+    }
+
+    GfVec4f vp = renderPassState->GetViewport();
+
+    // Determine whether we need to update the renderer camera.
+    GfMatrix4d view = renderPassState->GetWorldToViewMatrix();
+    GfMatrix4d proj = renderPassState->GetProjectionMatrix();
+    if (_viewMatrix != view || _projMatrix != proj) {
+        _viewMatrix = view;
+        _projMatrix = proj;
+
+        _renderThread->StopRender();
+        _renderer->SetCamera(_viewMatrix, _projMatrix);
+        needStartRender = true;
+    }
+
+    // Determine whether we need to update the renderer viewport.
+    if (_width != vp[2] || _height != vp[3]) {
+        _width = vp[2];
+        _height = vp[3];
+
+        _renderThread->StopRender();
+        _renderer->SetViewport(_width, _height);
+        _colorBuffer.Allocate(GfVec3i(_width, _height, 1), HdFormatUNorm8Vec4,
+                              /*multiSampled=*/true);
+        _depthBuffer.Allocate(GfVec3i(_width, _height, 1), HdFormatFloat32,
+                              /*multiSampled=*/false);
+        needStartRender = true;
+    }
+
+    // Determine whether we need to update the renderer AOV bindings.
+    //
+    // It's possible for the passed in bindings to be empty, but that's
+    // never a legal state for the renderer, so if that's the case we add
+    // a color and depth aov that we can blit to the GL framebuffer.
+    //
+    // If the renderer AOV bindings are empty, force a bindings update so that
+    // we always get a chance to add color/depth on the first time through.
+    HdRenderPassAovBindingVector aovBindings =
+        renderPassState->GetAovBindings();
+    if (_aovBindings != aovBindings || _renderer->GetAovBindings().empty()) {
+        _aovBindings = aovBindings;
+
+        _renderThread->StopRender();
+        if (aovBindings.size() == 0) {
+            // No attachment means we should render to the GL framebuffer
+            HdRenderPassAovBinding colorAov;
+            colorAov.aovName = HdAovTokens->color;
+            colorAov.renderBuffer = &_colorBuffer;
+            colorAov.clearValue =
+                VtValue(GfVec4f(0.0707f, 0.0707f, 0.0707f, 1.0f));
+            aovBindings.push_back(colorAov);
+            HdRenderPassAovBinding depthAov;
+            depthAov.aovName = HdAovTokens->depth;
+            depthAov.renderBuffer = &_depthBuffer;
+            depthAov.clearValue = VtValue(1.0f);
+            aovBindings.push_back(depthAov);
+        }
+        _renderer->SetAovBindings(aovBindings);
+        // In general, the render thread clears aov bindings, but make sure
+        // they are cleared initially on this thread.
+        _renderer->Clear();
+        needStartRender = true;
+    }
+
+    // If there are no AOVs specified, we should blit our local color buffer to
+    // the GL framebuffer.
+    if (_aovBindings.size() == 0) {
+        _converged = _colorBuffer.IsConverged() && _depthBuffer.IsConverged();
+        // To reduce flickering, don't update the compositor until every pixel
+        // has a sample (as determined by depth buffer convergence).
+        if (_depthBuffer.IsConverged()) {
+            _colorBuffer.Resolve();
+            uint8_t *cdata = reinterpret_cast<uint8_t*>(_colorBuffer.Map());
+            if (cdata) {
+                _compositor.UpdateColor(_width, _height, cdata);
+                _colorBuffer.Unmap();
+            }
+            _depthBuffer.Resolve();
+            uint8_t *ddata = reinterpret_cast<uint8_t*>(_depthBuffer.Map());
+            if (ddata) {
+                _compositor.UpdateDepth(_width, _height, ddata);
+                _depthBuffer.Unmap();
+            }
+        }
+
+        // LuxCore does not output opacity at this point so we disable alpha
+        // blending in the compositor so we can see the background color.
+/*
+        GLboolean restoreblendEnabled;
+        glGetBooleanv(GL_BLEND, &restoreblendEnabled);
+        glDisable(GL_BLEND);
+*/
+
+        _compositor.Draw();
+
+/*
+        if (restoreblendEnabled) {
+            glEnable(GL_BLEND);
+        }
+*/
+    }
+
+    // Only start a new render if something in the scene has changed.
+    if (needStartRender) {
+        _converged = false;
+        _renderer->MarkAovBuffersUnconverged();
+        _renderThread->StartRender();
+    }
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
