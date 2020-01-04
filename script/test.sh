16b8787f33939b527f842bdb21ef146a37a5c1c2
John Gann <jbgann@gmail.com>
2020-01-03 17:47:27 -0800

    Add CentOS 7 build support

---
+++ script/test.sh
@@ -0,0 +1,2 @@
+python ~/masters/USD-repo/extras/usd/tutorials/helloWorld/helloWorld.py
+~/masters/USD/bin/usdview --renderer LuxCore ~/masters/USD-repo/extras/usd/tutorials/helloWorld/HelloWorld.usda
