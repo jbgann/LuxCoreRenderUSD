54e7ccf6335258f02e76d565bfdef1b1de46f704
John Gann <jbgann@gmail.com>
2020-03-16 17:36:05 -0700

    Adjust test script to use the geo test asset

--- script/test.bat
+++ script/test.bat
@@ -1,1 +1,2 @@
-usdview --renderer LuxCore c:\masters\USD\extras\usd\tutorials\helloworld\helloworld.usda
+usdview --renderer LuxCore test\asset\geo_GridSphereLightCam.004.usd_scene.usda
+rem usdview test\asset\geo_GridSphereLightCam.004.usd_scene.usda
