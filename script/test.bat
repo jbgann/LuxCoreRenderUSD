7764cc072c4fabd5beda25da3a2cc92c8b880f75
John Gann <jbgann@gmail.com>
2020-07-01 23:58:39 -0700

    Change complexity test setting to high

--- script/test.bat
+++ script/test.bat
@@ -1,4 +1,1 @@
-usdview --renderer LuxCore test\asset\geo_GridSphereLightCam.004.usd_scene.usda
-rem usdview test\asset\geo_GridSphereLightCam.004.usd_scene.usda
-rem usdview --renderer LuxCore test\asset\helloworld_instances.usda
-rem usdview test\asset\geo_GridSphereLightCam.004.usd_scene.usda
+usdview --renderer LuxCore --camera camera1 --complexity high test\asset\geo_GridSphereLightCam.004.usd_scene.usda
